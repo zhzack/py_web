7a7b9de36b2d78319ad0e1c6932b4ff54f62aa82
zhengzhi <v-zhengzhi@immotors.com>
2026-01-04 09:34:46 +0800

    del vue code

--- vue-project/src/api/http.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const http = axios.create({
-    baseURL: "/api", // 关键：走 Vite proxy
-    timeout: 10000,
-});
-
-export default http;
--- vue-project/src/api/scripts.js
+++
@@ -1,9 +0,0 @@
-import http from "./http";
-
-export function listScripts() {
-    return http.get("/v1/scripts/list");
-}
-
-export function runScript(name) {
-    return http.post("/v1/scripts/run", { name });
-}
